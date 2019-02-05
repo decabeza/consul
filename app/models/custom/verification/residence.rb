6b804ac7b6ddd4da2fd0488733a3016eaa64d9a6
tom <tom@toms-iMac.local>
2019-02-05 14:12:02 +0000

    Amend Verification Model

--- app/models/custom/verification/residence.rb
+++ app/models/custom/verification/residence.rb
@@ -1,29 +1,33 @@
-
-require_dependency Rails.root.join('app', 'models', 'verification', 'residence').to_s
+require_dependency Rails.root.join("app", "models", "verification", "residence").to_s
 
 class Verification::Residence
+  clear_validators!
 
-  validate :postal_code_in_madrid
-  validate :residence_in_madrid
+  validates :date_of_birth, presence: true
+  validates :terms_of_service, acceptance: { allow_nil: false }
+  validates :postal_code, presence: true
 
-  def postal_code_in_madrid
-    errors.add(:postal_code, I18n.t('verification.residence.new.error_not_allowed_postal_code')) unless valid_postal_code?
-  end
-
-  def residence_in_madrid
-    return if errors.any?
-
-    unless residency_valid?
-      errors.add(:residence_in_madrid, false)
-      store_failed_attempt
-      Lock.increase_tries(user)
-    end
-  end
+  validate :allowed_age
+  validate :postal_code_is_allowed
+  validate :document_number_uniqueness_if_present
 
   private
 
-    def valid_postal_code?
-      postal_code =~ /^280/
+    def retrieve_census_data
+      # Empty method because we don't check against census
     end
 
+    def postal_code_is_allowed
+      errors.add(:postal_code, I18n.t("verification.residence.new.error_not_allowed_postal_code")) unless valid_postal_code?
+    end
+
+    def valid_postal_code?
+      Zipcode.pluck(:code).include?(postal_code.upcase)
+    end
+
+    def document_number_uniqueness_if_present
+      if document_number.present?
+        document_number_uniqueness
+      end
+    end
 end
