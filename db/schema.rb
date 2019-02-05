ccffe523c18c4246d8f30f98e2aa9015a7d4d0aa
tom <tom@toms-iMac.local>
2019-02-05 13:47:51 +0000

    Add postcode table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190103132925) do
+ActiveRecord::Schema.define(version: 20190205134601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1532,6 +1532,12 @@ ActiveRecord::Schema.define(version: 20190103132925) do
     t.integer  "limit",      default: 3
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+  end
+
+  create_table "zipcodes", force: :cascade do |t|
+    t.string   "code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "administrators", "users"
