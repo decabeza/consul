257bd9ae8d99a2b4608b1a7e860cf540d45a2a92
tom <tom@toms-iMac.local>
2019-02-07 11:21:18 +0000

    Fix for verification form

--- app/models/user.rb
+++ app/models/user.rb
@@ -36,7 +36,7 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, if: :username_required?
   validates :username, uniqueness: { scope: :registering_with_oauth }, if: :username_required?
-  validates :document_number, uniqueness: { scope: :document_type }, allow_nil: true
+  validates :document_number, uniqueness: { scope: :document_type }, allow_blank: true
 
   validate :validate_username_length
 
